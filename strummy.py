be1aafae3ee234149a6ba4c1630d3080de6d0ec0
ngaragulya <ngaragulya@gmail.com>
2020-06-27 16:45:52 -0400

    update

--- strummy.py
+++ strummy.py
@@ -8,4 +8,4 @@ async def on_ready():
     print(f'{bot.user} has logged in.')
     bot.load_extension('cogs.music')
     
-bot.run('NzIyOTE0MDI4MjQyMDc1NzQ5.XveWeQ.lqYQcVubhAae4BKiqgdGpZxrYWQ')
+bot.run('NzIyOTE0MDI4MjQyMDc1NzQ5.Xvevlw.PHM4McpQaKXVnKCHZ8erI5O5VgU')
